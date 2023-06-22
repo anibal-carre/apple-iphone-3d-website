4962c7553068b492e90480c4916f5fe6ab065fbb
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-06-22 13:40:14 -0300

    last commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import DisplaySection from "./components/DisplaySection";
+import Jumbotron from "./components/Jumbotron";
+import Loader from "./components/Loader";
+import Nav from "./components/Nav";
+import SoundSection from "./components/SoundSection";
+import WebgiViewer from "./components/WebgiViewer";
+import { useRef } from "react";
+
+function App() {
+  const WebgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    WebgiViewerRef.current.triggerPreview();
+  };
+
+  return (
+    <div className="App">
+      <Loader />
+      <div ref={contentRef} id="content">
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+
+      <WebgiViewer contentRef={contentRef} ref={WebgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
